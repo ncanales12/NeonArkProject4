487ef7064a66c71f37f0b0d4eba01804616839e7
ncanales12 <nicolas.canales@g.austincc.edu>
2026-05-04 21:29:36 -0400

    Added comments to Warden model

--- src/org/example/neonark/model/Warden.java
+++ src/org/example/neonark/model/Warden.java
@@ -1,6 +1,11 @@
 package org.example.neonark.model;
 
+/*
+ This class represents a single Warden.
+ It just holds data that comes from the CSV file.
+*/
 public class Warden {
+
     private int wardenId;
     private String firstName;
     private String lastName;
@@ -13,10 +18,14 @@ public class Warden {
     private String startDate;
     private String endDate;
 
+    /*
+     Constructor used to create a Warden object from CSV data.
+    */
     public Warden(int wardenId, String firstName, String lastName, String email,
                   String identifierType, String identifierValue, String role,
                   String employmentStatus, String clearanceLevel,
                   String startDate, String endDate) {
+
         this.wardenId = wardenId;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -29,6 +38,8 @@ public class Warden {
         this.startDate = startDate;
         this.endDate = endDate;
     }
+
+    // getters below just return each field
 
     public int getWardenId() {
         return wardenId;
