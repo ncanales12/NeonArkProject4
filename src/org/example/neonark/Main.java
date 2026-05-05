60b46977fa8f43ea628168363b04372726c4ea17
ncanales12 <nicolas.canales@g.austincc.edu>
2026-05-04 21:27:29 -0400

    Added file-level comments to Main.java

--- src/org/example/neonark/Main.java
+++ src/org/example/neonark/Main.java
@@ -2,8 +2,16 @@ package org.example.neonark;
 
 import org.example.neonark.menu.MainMenu;
 
+/*
+ This is the main entry point for the program.
+ It just starts the menu system.
+ Nothing complicated happens here.
+*/
 public class Main {
+
     public static void main(String[] args) {
+
+        // create menu and start the app
         MainMenu menu = new MainMenu();
         menu.start();
     }
